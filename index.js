9825edc9432818d8e05170f4ca1792ffecf9ac91
Atharva <atharvajoshi202@gmail.com>
2022-08-05 00:43:27 +0530

    Moved Js to index.js

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ const scrollTop = () =>{
     topElement.scrollIntoView({behavior:"smooth"});
 }
 scrollElement.addEventListener("click",scrollTop);
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
