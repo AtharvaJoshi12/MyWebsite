5275c665215b4a4c16cb0981b12094a29a2d0c42
Atharva <atharvajoshi202@gmail.com>
2022-08-05 00:54:56 +0530

    Slider Modifed for mobile view.

--- index.js
+++ index.js
@@ -21,3 +21,45 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+
+
+
+
+function myFunction(widthSize) {
+  if (widthSize.matches) {
+      // If media query matches
+      const swiper = new Swiper(".swiper", {
+          slidesPerView: 1,
+          spaceBetween: 30,
+          autoplay: {
+              delay: 2500,
+              disableOnInteraction: false,
+          },
+
+          pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+          },
+      });
+  } else {
+      const swiper = new Swiper(".swiper", {
+          slidesPerView: 2,
+          spaceBetween: 30,
+          autoplay: {
+              delay: 2500,
+              disableOnInteraction: false,
+          },
+          pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+          },
+      });
+  }
+}
+
+const widthSize = window.matchMedia("(max-width: 780px)");
+// Call listener function at run time
+myFunction(widthSize);
+// Attach listener function on state changes
+widthSize.addListener(myFunction);
